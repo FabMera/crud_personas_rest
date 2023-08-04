32630000fb5f42bfa86ab7b658da0559263ccc4f
FabMera <fabianmeraromero@gmail.com>
2023-08-03 21:56:58 -0400

    swagger

--- src/main/java/com/fmera/jwt/backend/BackendApplication.java
+++ src/main/java/com/fmera/jwt/backend/BackendApplication.java
@@ -1,13 +1,27 @@
 package com.fmera.jwt.backend;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Backend API Personas CRUD")
+                        .version("1.0")
+                        .description("CRUD de Personas")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0")
+                                .url("https://springdoc.org")));
+    }
 }
